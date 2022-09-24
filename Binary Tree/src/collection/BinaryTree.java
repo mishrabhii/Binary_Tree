77d50461a277f236ea2e33a3774440ed7550074a
mishrabhii <abhi.mishra509@gmail.com>
2022-09-24 13:52:00 +0530

    Adding new elements in left side of the node

--- Binary Tree/src/collection/BinaryTree.java
+++ Binary Tree/src/collection/BinaryTree.java
@@ -9,9 +9,20 @@ public class BinaryTree<K extends Comparable<K>> {
 		binaryTree.add(56);
 		binaryTree.add(30);
 		binaryTree.add(70);
+		binaryTree.add(22);
+		binaryTree.add(40);
+		binaryTree.add(60);
+		binaryTree.add(95);
+		binaryTree.add(11);
+		binaryTree.add(65);
+		binaryTree.add(3);
+		binaryTree.add(16);
+		binaryTree.add(63);
+		binaryTree.add(67);
 
 		System.out.println("\nBinary Tree Size = " + binaryTree.getSize() + "\n");
 		binaryTree.printTree();
+		
 	}
 	private BinaryNode<K> root;
 
